0aac7146f23569de4e4f05fbb6cf09f3396ad63f
wangX <wangX@qq.com>
2019-07-22 11:40:37 +0800

    2019年7月22日11点38分第一次收集静态文件，第一次迁移models

--- FreshShop/FreshShop/settings.py
+++ FreshShop/FreshShop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'FreshShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
