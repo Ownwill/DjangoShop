6276f407d8897765d8ed3fb76d2fbd87eb980319
wangX <wangX@qq.com>
2019-07-22 14:09:25 +0800

    2019年7月22日14点06分，前端可以链接到css，和images，前端显示出样式

--- FreshShop/Store/urls.py
+++ FreshShop/Store/urls.py
@@ -3,4 +3,14 @@ from Store.views import *
 
 urlpatterns = [
     path('index/',index),
+    path('cart/',cart),
+    path('detail/',detail),
+    path('list/',list),
+    path('login/',login),
+    path('place_order/',place_order),
+    path('register/',register),
+    path('user_center_info/',user_center_info),
+    path('user_center_order/',user_center_order),
+    path('user_center_site/',user_center_site),
+
 ]
--- FreshShop/Store/views.py
+++ FreshShop/Store/views.py
@@ -5,3 +5,35 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request,'store/index.html')
+
+def cart(request):
+    return render(request,'store/cart.html')
+
+def detail(request):
+    return render(request,'store/detail.html')
+
+def list(request):
+    return render(request,'store/list.html')
+
+def login(request):
+    return render(request,'store/login.html')
+
+def place_order(request):
+    return render(request,'store/place_order.html')
+
+def register(request):
+    return render(request,'store/register.html')
+
+def user_center_info(request):
+    return render(request,'store/user_center_info.html')
+
+def user_center_order(request):
+    return render(request,'store/user_center_order.html')
+
+def user_center_site(request):
+    return render(request,'store/user_center_site.html')
+
+
+
+
+
