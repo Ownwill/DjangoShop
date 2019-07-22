1ba83859768b1179e8284a726ba9fa2f176cdb49
wangX <wangX@qq.com>
2019-07-22 21:37:45 +0800

    2019年7月22日21点36分，后台页面模板继承完成，后台页面的样式配置完成，登录注册搭起架子。

--- FreshShop/Store/urls.py
+++ FreshShop/Store/urls.py
@@ -2,15 +2,19 @@ from django.urls import path,include
 from Store.views import *
 
 urlpatterns = [
-    path('index/',index),
-    path('cart/',cart),
-    path('detail/',detail),
-    path('list/',list),
-    path('login/',login),
-    path('place_order/',place_order),
     path('register/',register),
-    path('user_center_info/',user_center_info),
-    path('user_center_order/',user_center_order),
-    path('user_center_site/',user_center_site),
-
+    path('login/',login),
+    path('index/', index),
+    path('page_404/',page_404),
+    path('base/',base),
+    path('blank/', blank),
+    path('buttons/',buttons),
+    path('cards/',cards),
+    path('charts/', charts),
+    path('forgotPwd/',forgotPwd),
+    path('tables/',tables),
+    path('utiAni/',utiAni),
+    path('utiBor/',utiBor),
+    path('utiCol/',utiCol),
+    path('utiOth/',utiOth),
 ]
--- FreshShop/Store/views.py
+++ FreshShop/Store/views.py
@@ -1,37 +1,113 @@
+import hashlib
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
+
+from Store.models import *
 # Create your views here.
 
+def set_password(password):
+    md5 = hashlib.md5()
+    md5.update(password.encode())
+    return md5.hexdigest()
+
+def UserVaild(func):
+    def inner(request,*args,**kwargs):
+        u_cookies = request.COOKIES.get('username')
+        u_session = request.session.get('username')
+        if u_cookies and u_session and u_cookies == u_session:
+            return func(request,*args,**kwargs)
+    return inner
+
+def register(request):
+    """
+    返回注册页面，进行注册数据保存
+    :param request:
+    :return:
+    """
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username and password:
+            user = Seller()
+            user.username = username
+            user.password = set_password(password)
+            user.save()
+            return HttpResponseRedirect('/store/login/')
+    return render(request,'store/register.html')
+
+def login(request):
+    """
+    登录成功跳转到index页面，未登录成功跳转到登录页
+    :param request:
+    :return:
+    """
+    response = render(request,'store/login.html')
+    response.set_cookie("login_from","login_page")  #设置cookies说明来源，
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get('password')
+        if username and password: #前台是否传来账号密码
+            user = Seller.objects.filter(username=username).first()
+            if user: #判断用户是否存在于数据库
+                web_password = set_password(password)
+                print(web_password)
+                cookies = request.COOKIES.get('login_from')
+                if user.password == web_password and cookies == "login_page": #判断密码是否正确和来源
+                    response = HttpResponseRedirect('/store/index/')
+                    response.set_cookie('username',username)
+                    request.session['username'] = username
+                    return response
+    return response
+
+@UserVaild
 def index(request):
     return render(request,'store/index.html')
 
-def cart(request):
-    return render(request,'store/cart.html')
+def page_404(request):
+    return render(request,'store/404.html')
 
-def detail(request):
-    return render(request,'store/detail.html')
+def base(request):
+    return render(request,'store/base.html')
 
-def list(request):
-    return render(request,'store/list.html')
+def blank(request):
+    return render(request,'store/blank.html')
 
-def login(request):
-    return render(request,'store/login.html')
+def buttons(request):
+    return render(request,'store/buttons.html')
 
-def place_order(request):
-    return render(request,'store/place_order.html')
+def cards(request):
+    return render(request,'store/cards.html')
 
-def register(request):
-    return render(request,'store/register.html')
+def charts(request):
+    return render(request,'store/charts.html')
 
-def user_center_info(request):
-    return render(request,'store/user_center_info.html')
+def forgotPwd(request):
+    return render(request,'store/forgot-password.html')
 
-def user_center_order(request):
-    return render(request,'store/user_center_order.html')
+def tables(request):
+    return render(request,'store/tables.html')
 
-def user_center_site(request):
-    return render(request,'store/user_center_site.html')
+def utiAni(request):
+    return render(request,'store/utilities-animation.html')
+
+def utiBor(request):
+    return render(request,'store/utilities-border.html')
+
+def utiCol(request):
+    return render(request,'store/utilities-color.html')
+
+def utiOth(request):
+    return render(request,'store/utilities-other.html')
+
+
+
+
+
+
+
 
 
 
